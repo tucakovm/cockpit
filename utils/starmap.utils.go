1291176506404876f880e5001e63d504d41cb824
Milan Tucakov <milan.tucakov.99@gmail.com>
2026-01-30 17:20:43 +0100

    Update README.md

--- utils/starmap.utils.go
+++ utils/starmap.utils.go
@@ -3,6 +3,10 @@ package utils
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
 func ReadStarmapFile(filePath string) (map[string]interface{}, error) {
@@ -12,8 +16,25 @@ func ReadStarmapFile(filePath string) (map[string]interface{}, error) {
 	}
 
 	var result map[string]interface{}
-	if err := json.Unmarshal(data, &result); err != nil {
-		return nil, err
+
+	ext := strings.ToLower(filepath.Ext(filePath))
+
+	switch ext {
+	case ".yaml", ".yml":
+		if err := yaml.Unmarshal(data, &result); err != nil {
+			return nil, err
+		}
+	case ".json":
+		if err := json.Unmarshal(data, &result); err != nil {
+			return nil, err
+		}
+	default:
+
+		if err := yaml.Unmarshal(data, &result); err != nil {
+			if jsonErr := json.Unmarshal(data, &result); jsonErr != nil {
+				return nil, err
+			}
+		}
 	}
 
 	return result, nil
